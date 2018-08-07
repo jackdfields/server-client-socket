dbfbfaabc49746de283d1c9c08cbccc688c8cb9b
jackdfields <42076428+jackdfields@users.noreply.github.com>
2018-08-07 11:34:41 -0700

    Bug fix
    need to change the While True: because it creates infinite loops, while i just want this to be called recursively when the files calls it

--- dir_scanner.py
+++ dir_scanner.py
@@ -5,15 +5,15 @@ import datetime
 
 def path_walker(path):
         current_time = datetime.datetime.now()
-	count = 0
-        while True:
-                print("Current time is:" + str(current_time) +
+        count = 0
+        print("Current time is:" + str(current_time) +
                       "\n -----------------------------")
-                for (files) in os.walk(path):
-                        for filename in files:
-				count += 1
-                                print(count)
+        for (root, dir, files) in os.walk(path):
+                for filename in files:
+                        count += 1
+                        return count
+                         
                                 
 if __name__ == '__main__':
         # place r before file path to convert into raw string
-	path_walker(r'path for files')
+	path_walker(r'path_for_files')
