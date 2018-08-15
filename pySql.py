3c8e8180ca2289f05b7d919decea0e5d86be64f6
jackdfields <42076428+jackdfields@users.noreply.github.com>
2018-08-15 16:03:32 -0700

    Html support
    This update has added in "Time since last check in", color warnings to the table.
    These color warnings will trigger if a computer hasn't checked in within 5 minutes, if the files in a folder have built up to over 10, or both.

--- pySql.py
+++ pySql.py
@@ -1,7 +1,8 @@
-#pypyodbc connection to SQL
+# pypyodbc connection to SQL
 
 import pypyodbc
 import csv
+import datetime
 
 class PySQLTrans():
     """ send client data to database """
@@ -11,10 +12,10 @@ class PySQLTrans():
 
         
     def sql_db_upload(self):    
-        #Insert Values to the Database
-        #establish connection
+        # Insert Values to the Database
+        # establish connection
         cxcn = pypyodbc.connect('Driver={SQL Server};'
-                                'Server=servername;'
+                                'Server=CASHLAB-001901\LOCSMS;'
                                 'Database=STORESQL;'
                                 'Trusted_Connection=yes;')
 
@@ -26,47 +27,108 @@ class PySQLTrans():
 
         cursor.execute(SQLCommand, Values)
 
-        #save to DB
+        # save to DB
         cxcn.commit()
-        #close connection
+        # close connection
         cxcn.close()
 
     def sql_db_dowload(self):
         # Retrieve Values from DB
-        #EST new connection
+        # EST new connection
         cxcn = pypyodbc.connect('Driver={SQL Server};'
-                                'Server=servername;'
+                                'Server=CASHLAB-001901\LOCSMS;'
                                 'Database=STORESQL;'
                                 'Trusted_Connection=yes;')
 
-        #create cursor
+        # create cursor
         cursor = cxcn.cursor()
-        
+
+        # Order the DB results by ABC order AND most recently updated
         SQLCommand = ('SELECT ComputerName, NumOfFiles, Tran_Date FROM CLIENT_MONITOR\n'+
-                      'ORDER BY ComputerName;')
+                      'ORDER BY ComputerName ASC, Tran_Date DESC;')
 
         cursor.execute(SQLCommand)
-        #Fetch results
+        # Fetch results
         results = cursor.fetchall()
 
-        #Write to a file
+        # Write to a file
         with open('report_dialogue.txt', 'w') as file:
             for row in results:
-               C_name, N_files, T_date = row
-               actual_time,removed = str(T_date).split('.')
-               report_dialogue = (C_name , N_files, actual_time)
-               
-               file.write('  <tr>\n' +
-                          '\t<td>' +
-                          C_name +
-                          '</td>\n' +
-                          '\t<td>' +
-                          N_files +
-                          '</td>\n' +
-                          '\t<td>' +
-                          actual_time +
-                          '</td>\n</tr>\n')
+               Comp_name, Num_files, T_date = row
 
-        #close connection
+               # get time from DB and convert it ms for difference
+               db_milliseconds = int(round(T_date.timestamp() * 1000))
+               now_milliseconds = int(round(datetime.datetime.now().timestamp() * 1000))
+               current_time_lapse = now_milliseconds - db_milliseconds
+               # convert to datetime
+               time_since_checkin = round(current_time_lapse/60000)
+               
+               updated_time = str(datetime.datetime.fromtimestamp(db_milliseconds/1000).strftime('%Y-%m-%d %H:%M:%S'))
+
+               # Notice a lack of check-in 
+               if time_since_checkin > 5:
+                   file.write('  <tr>\n' +
+                              '\t<td align=center>' +
+                              Comp_name +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              Num_files +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              updated_time +
+                              '</td>\n' +
+                              '\t<td bgcolor=#FF0000; align=center>' +
+                              str(time_since_checkin) +
+                              '</td>\n</tr>\n')
+                   
+               # check if files aren't updating
+               elif int(Num_files) > 10:
+                   file.write('  <tr>\n' +
+                              '\t<td align=center>' +
+                              Comp_name +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              Num_files +
+                              '</td>\n' +
+                              '\t<td bgcolor=#FF0000; align=center>' +
+                              updated_time +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              str(time_since_checkin) +
+                              
+                              '</td>\n</tr>\n')
+                   
+               # check if files and check-in are not coming in
+               elif int(Num_files) > 10 and time_since_checkin > 5:
+                   file.write('  <tr>\n' +
+                              '\t<td align=center>' +
+                              Comp_name +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              Num_files +
+                              '</td>\n' +
+                              '\t<td bgcolor=#FF0000; align=center>' +
+                              updated_time +
+                              '</td>\n' +
+                              '\t<td bgcolor=#FF0000; align=center>' +
+                              str(time_since_checkin) +
+                              '</td>\n</tr>\n')
+
+               # write normally
+               else:
+                   file.write('  <tr>\n' +
+                              '\t<td align=center>' +
+                              Comp_name +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              Num_files +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              updated_time +
+                              '</td>\n' +
+                              '\t<td align=center>' +
+                              str(time_since_checkin) +
+                              '</td>\n</tr>\n')
+                    
+        # close connection
         cxcn.close()
- 
